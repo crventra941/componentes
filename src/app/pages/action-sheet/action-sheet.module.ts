7c032f382d800230a08039b7731b0aaf54ae7070
christian <christian@girasolo.com>
2019-04-29 13:49:36 -0500

    initial commit to 03-componentes

---
+++ src/app/pages/action-sheet/action-sheet.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+import { ComponentsModule } from '../../components/components.module';
+
+
+
+import { ActionSheetPage } from './action-sheet.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ActionSheetPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ComponentsModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [ActionSheetPage]
+})
+export class ActionSheetPageModule {}
